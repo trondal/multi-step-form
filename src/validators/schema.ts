07ae9e70a2f387e82a025686580ab2e58b118072
Trond Albinussen <trond.albinussen@skatteetaten.no>
2026-03-11 15:12:09 +0100

    Can successfully upload

--- src/validators/schema.ts
+++ src/validators/schema.ts
@@ -1,13 +1,21 @@
 import { z } from 'zod';
 
+const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
+const ACCEPTED_IMAGE_TYPES = [
+  'image/jpeg',
+  'image/jpg',
+  'image/png',
+  'image/webp'
+];
+
 export const step1Schema = z.object({
-  email: z.string().min(2, { error: 'Please enter a valid email address' }),
   firstName: z
     .string()
     .min(3, { error: 'First name must be at least 3 characters' }),
   lastName: z
     .string()
-    .min(3, { error: 'Last name must be at least 3 characters' })
+    .min(3, { error: 'Last name must be at least 3 characters' }),
+  email: z.string().min(2, { error: 'Please enter a valid email address' })
 });
 
 export const step2Schema = z.object({
@@ -26,20 +34,28 @@ export const step2Schema = z.object({
 });
 
 export const step3Schema = z.object({
-  fil: z
-    .file()
-    //.refine((files) => files.length === 0, 'At least one file i required')
-    .max(1024 * 1024)
-    .mime('image/png', { error: 'Only png files' })
+  file: z
+    .custom<FileList>((val) => val instanceof FileList, 'Required')
+    .refine((files) => files.length > 0, 'At least one file is required.')
+
+    .refine(
+      (files) => files.length <= 5,
+      'You can upload a maximum of 5 files.'
+    )
+    .refine((files) => {
+      return Array.from(files).every((file) => file.size <= MAX_FILE_SIZE);
+    }, `Each file must be less than 5MB.`)
+    .refine((files) => {
+      return Array.from(files).every((file) =>
+        ACCEPTED_IMAGE_TYPES.includes(file.type)
+      );
+    }, 'Only .jpg, .jpeg, .png and .webp formats are supported.')
 });
 
 export const step4Schema = z.object({
-  cardNumber: z
-    .string()
-    .min(3, { error: 'CardNumber must be at least 3 characters' }),
-  /*cardNumber: z.string().regex(/^[0-9]{16}$/, {
+  cardNumber: z.string().regex(/^[0-9]{16}$/, {
     error: 'Please enter a valid 16-digit card number'
-  }),*/
+  }),
   cardholderName: z
     .string()
     .min(2, { error: 'Cardholder name must be at least 2 characters' })
@@ -50,7 +66,7 @@ export const step4Schema = z.object({
 export const CombinedCheckoutSchema = z.object({
   ...step1Schema.shape,
   ...step2Schema.shape,
-  //...step3Schema.shape,
+  ...step3Schema.shape,
   ...step4Schema.shape
 });
 
