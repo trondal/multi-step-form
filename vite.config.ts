9d2bb20765646f5811fdba2589c5050e5904bd2b
Trond Albinussen <trond.albinussen@skatteetaten.no>
2026-03-18 09:15:23 +0100

    vite@8

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest/config" />
 
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
